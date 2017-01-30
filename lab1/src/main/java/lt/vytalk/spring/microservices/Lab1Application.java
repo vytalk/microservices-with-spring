4552f546a4b472576455d8d9756554be95530c06
vytalk <vytautas.alkimavicius@gmail.com>
2017-01-30 22:30:46 +0200

    lab 1 initial commit

---
+++ lab1/src/main/java/lt/vytalk/spring/microservices/Lab1Application.java
@@ -0,0 +1,12 @@
+package lt.vytalk.spring.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Application.class, args);
+	}
+}
