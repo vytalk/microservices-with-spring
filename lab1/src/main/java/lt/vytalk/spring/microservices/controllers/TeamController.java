800d1dca7914a1f8efad42576b20ac6c2755256d
vytalk <vytautas.alkimavicius@gmail.com>
2017-02-02 22:00:54 +0200

    lab1 part6

--- lab1/src/main/java/lt/vytalk/spring/microservices/controllers/TeamController.java
+++ lab1/src/main/java/lt/vytalk/spring/microservices/controllers/TeamController.java
@@ -5,9 +5,7 @@ import lt.vytalk.spring.microservices.repos.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class TeamController {
 
     @Autowired
--- lab1/src/main/java/lt/vytalk/spring/microservices/repos/PlayerRepository.java
+++ lab1/src/main/java/lt/vytalk/spring/microservices/repos/PlayerRepository.java
@@ -1,9 +1,9 @@
 package lt.vytalk.spring.microservices.repos;
 
-import lt.vytalk.spring.microservices.model.Team;
+import lt.vytalk.spring.microservices.model.Player;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RestResource(path = "teams", rel="team")
-public interface TeamRepository extends CrudRepository<Team, Long> {
+@RestResource(path = "players", rel="player")
+public interface PlayerRepository extends CrudRepository<Player, Long>{
 }
